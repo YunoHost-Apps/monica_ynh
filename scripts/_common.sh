eef0303278cb6e525642d46c83b065253b76998e
anmol26s <5068843+anmol26s@users.noreply.github.com>
2020-04-29 23:28:50 +0530

    Updated to version 2.17.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
-source helpers/ynh_add_extra_apt_repos__3
-source helpers/ynh_install_php__3
-source helpers/ynh_composer__2
 
-# =============================================================================
-#                     YUNOHOST 2.7 FORTHCOMING HELPERS
-# =============================================================================
-
-# Create a dedicated php-fpm config
-#
-# usage: ynh_add_fpm_config
-ynh_add_fpm7.2_config () {
-  ynh_add_fpm_config --phpversion="7.2"
-}
-
-#
-# Composer helpers
-#
-
-# Install and initialize Composer in the given directory
-# usage: init_composer
-init_composer() {
-  ynh_install_composer --phpversion="7.2" --workdir="$final_path"
-
-  # update dependencies to create composer.lock
-  ynh_composer_exec --phpversion="7.2" --workdir="$final_path" --commands="install --no-dev --prefer-dist" \
-    || ynh_die "Unable to update monica core dependencies"
-}
-
-#
-# PHP7 helpers
-#
 pkg_dependencies="php7.2-cli php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-bcmath php7.2-intl php7.2-xml php7.2-curl php7.2-gd php7.2-gmp"
 
-ynh_install_php7.2 () {
-  ynh_install_php --phpversion="7.2" --package="$pkg_dependencies"
+
+# Execute a command with Composer
+#
+# usage: ynh_composer_exec --phpversion=phpversion [--workdir=$final_path] --commands="commands"
+# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
+# | arg: -c, --commands - Commands to execute.
+ynh_composer_exec () {
+	# Declare an array to define the options of this helper.
+	local legacy_args=vwc
+	declare -Ar args_array=( [v]=phpversion= [w]=workdir= [c]=commands= )
+	local phpversion
+	local workdir
+	local commands
+	# Manage arguments with getopts
+	ynh_handle_getopts_args "$@"
+	workdir="${workdir:-$final_path}"
+	phpversion="${phpversion:-7.0}"
+
+	COMPOSER_HOME="$workdir/.composer" \
+		php${phpversion} "$workdir/composer.phar" $commands \
+		-d "$workdir" --quiet --no-interaction
+}
+
+# Install and initialize Composer in the given directory
+#
+# usage: ynh_install_composer --phpversion=phpversion [--workdir=$final_path]
+# | arg: -w, --workdir - The directory from where the command will be executed. Default $final_path.
+ynh_install_composer () {
+	# Declare an array to define the options of this helper.
+	local legacy_args=vw
+	declare -Ar args_array=( [v]=phpversion= [w]=workdir= )
+	local phpversion
+	local workdir
+	# Manage arguments with getopts
+	ynh_handle_getopts_args "$@"
+	workdir="${workdir:-$final_path}"
+	phpversion="${phpversion:-7.0}"
+
+	curl -sS https://getcomposer.org/installer \
+		| COMPOSER_HOME="$workdir/.composer" \
+		php${phpversion} -- --quiet --install-dir="$workdir" \
+		|| ynh_die "Unable to install Composer."
+
+	# update dependencies to create composer.lock
+	ynh_composer_exec --phpversion="${phpversion}" --workdir="$workdir" --commands="install --no-dev" \
+		|| ynh_die "Unable to update core dependencies with Composer."
 }
