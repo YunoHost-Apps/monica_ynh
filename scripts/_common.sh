4b497cf6445fc515070265721f40ceb816864bf1
anmol26s <5068843+anmol26s@users.noreply.github.com>
2020-04-30 05:48:30 +0530

    update common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+readonly YNH_DEFAULT_PHP_VERSION=7.0
+# Declare the actual php version to use.
+# A packager willing to use another version of php can override the variable into its _common.sh.
+YNH_PHP_VERSION=${YNH_PHP_VERSION:-$YNH_DEFAULT_PHP_VERSION}
+
 
 pkg_dependencies="php7.2-cli php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-zip php7.2-bcmath php7.2-intl php7.2-xml php7.2-curl php7.2-gd php7.2-gmp"
 
