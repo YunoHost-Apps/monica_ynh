add68ee81686094c4d370c2eb9ea90c98c00adc6
ericgaspar <junk.eg@free.fr>
2022-05-30 15:13:06 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,11 +6,11 @@
 
 NODEJS_VERSION=16
 
-YNH_PHP_VERSION="7.4"
+YNH_PHP_VERSION="8.0"
 
-YNH_COMPOSER_VERSION="2.3.3"
+YNH_COMPOSER_VERSION="2.3.5"
 
-pkg_dependencies="php-redis php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-iconv php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip"
+pkg_dependencies="php${YNH_PHP_VERSION}-redis php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-iconv php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-mysqli php${YNH_PHP_VERSION}-opcache php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip"
 
 #=================================================
 # PERSONAL HELPERS
